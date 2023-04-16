6ca114ce82de47a6047d6a39ae361760729e1589
krishan93985 <krisharush93985@gmail.com>
2023-04-16 21:03:14 +0530

    updated route of chats

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Chats from './pages/Chats/Chats';
 
@@ -6,11 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className='background-image'></div>
-      <Routes>
-        <Route exact path='/chats' element={
-          <Chats />
-        } />
-      </Routes>
+      <Chats />
     </div>
   );
 }
